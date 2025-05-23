2132fba831c20b68cb21ef51d922d7a21834d115
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-05-23 14:20:06 +0100

    created the basic_operations python script and pushed to remote for the first time

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,21 @@
+# This is a  basic program that will carry out simple Arithmetic Operations using two numbers
+
+#initalize the two numbers
+
+number1 = 10
+number2 =  5 
+
+# Addition operations
+addition_result = number1 + number2
+
+
+# multiplication operation
+multiplication_result = number1 * number2
+
+#subtraction operation
+subtraction_result = number1 - number2
+
+print(f"Multiplication of {number1} and {number2} is {multiplication_result}")
+print(f"Addition of {number1} and {number2}  is {addition_result}")
+print(f"Difference of {number1} and {number2} is {subtraction_result}")
+
