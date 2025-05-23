bb65e70649c10ed8f75b89240df0a014a5c98376
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-05-23 14:57:36 +0100

    created a new script for calculating simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,20 @@
+# This is a python script that will compute the simple interest from a capital
+
+#( I ) is the interest earned,
+#( P ) is the principal amount (initial investment),
+#( R ) is the annual interest rate (as a decimal),
+#( T ) is the time the money is invested for in years.
+
+
+# Variable Declaration
+
+principal = 1000
+
+rate = 0.05 
+time = 3
+
+
+interest = (principal * rate * time)
+
+print(f"The simple interest is: {interest}")
+
