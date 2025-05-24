a014febbd177b0587afb39016b635621f2771515
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-05-24 04:13:49 +0100

    created a personal financal python script

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,49 @@
+# This is a python script that will manage a person's finances
+
+# Variable Declaration
+
+
+
+income = float(input("Enter your income: "))
+total_monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+
+# calculate monthly savings
+monthly_savings = income - total_monthly_expenses   
+
+#calculate projected yearly savings
+projected_yearly_savings = monthly_savings * 12
+
+#calculate projected yearly interest
+projected_yearly_interest = projected_yearly_savings * 0.05
+
+#calculate total projected savings
+total_projected_savings = projected_yearly_savings + projected_yearly_interest
+
+#display results
+print(f"Your monthly savings are, ${monthly_savings}")
+
+print(f"Projected savings after one year, with interest, is: ${total_projected_savings}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
