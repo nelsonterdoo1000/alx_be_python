6f54da5c74c8784fd951c60f34f4cc5ef72e35f6
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-05-24 02:00:35 +0100

    Created a python script that takes a users current age anc calculates how old they will be in 2050

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,13 @@
+# This is a python script that will calculate the future age of a person    
+
+# Variable Declaration
+
+current_age = int(input("How old are you? "))
+
+future_age = current_age + 27
+
+print(f"In 2050, you will be {future_age} years old")
+
+
+
+
