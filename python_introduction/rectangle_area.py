3bb88faed28c1e329fca0e952005cb639c3cb690
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-05-24 01:42:00 +0100

    Created a python script to calculate the area of a rectangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,11 @@
+# This is a python script that will compute the area of a rectangle
+
+# Variable Declaration
+
+length = 10
+width = 5
+
+area = length * width
+
+print(f"The area of the rectangle is: {area}")
+
