c79f5faa1201b3afec4c0857da2d22b1b74bdb85
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-01 23:15:54 +0100

    changed the code for multiplication table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,10 +1,10 @@
 # script to print the multiplication table of a number
 
-user_input = int(input("Enter a number to see its multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     #declare the product variable
-    product = user_input * i
+    product = number * i
     
     #print the product
-    print(f"{user_input} * {i} = {product}")
+    print(f"{number} * {i} = {product}")
