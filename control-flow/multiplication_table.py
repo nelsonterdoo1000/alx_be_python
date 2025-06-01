9d5123e4ef52a95ea0963a28baa38d1042d83031
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-01 20:37:49 +0100

    changed the order of the code to fit into what the checker is demanding

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,8 +1,6 @@
 # script to print the multiplication table of a number
 
-
-
 user_input = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
-    print(f"{i} * {user_input} =  {i * user_input}")
+    print(f"{user_input} * {i} = {user_input * i}")
