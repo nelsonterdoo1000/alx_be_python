20cae1064505458e68456523466387552da18242
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-08 21:34:37 +0100

    rewrote the code to meet the checker requirement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,19 @@
-def perform_operation(num1,num2,operation: str):
+def perform_operation(num1, num2, operation):
+    """
+    Perform arithmetic operation on two numbers.
+    
+    Args:
+        num1: First number
+        num2: Second number
+        operation: String indicating the operation ('+', '-', '*', '/')
+        
+    Returns:
+        Result of the arithmetic operation
+        
+    Raises:
+        ValueError: If operation is not supported
+        ZeroDivisionError: If division by zero is attempted
+    """
     if operation == '+':
         return num1 + num2
     elif operation == '-':
@@ -8,8 +23,6 @@ def perform_operation(num1,num2,operation: str):
     elif operation == '/':
         if num2 == 0:
             raise Exception("You cannot divide by zero")
-        else:
-            return num1 / num2
+        return num1 / num2
     else:
-        return "You did not enter a valid mathematic operator!"
-
+        return "You did not enter a valid mathematic operator!" 
