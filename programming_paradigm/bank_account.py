ab78dc0781bc5a11ad8288a8af565457a28468bb
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-15 23:39:14 +0100

    changed the code to meet with checker requirements

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,20 @@
 class BankAccount:
     def __init__(self, account_balance=0) -> None:
         self.account_balance = float(account_balance)
-
+    
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += float(amount)
             return f"Deposited: ${float(amount):.2f}"
         else:
             return "Invalid deposit amount"
-
+    
     def withdraw(self, amount):
-        if amount <= self.account_balance:
+        if amount > 0 and amount <= self.account_balance:
             self.account_balance -= float(amount)
-            return f"Withdrawn: ${float(amount):.2f}"
+            return True
         else:
-            return "Insufficient funds."
-
+            return False
+    
     def display_balance(self):
-        return f"Current Balance: ${self.account_balance:.2f}"
-
+        print(f"Current Balance: ${self.account_balance:.2f}")
