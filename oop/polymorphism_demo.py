c718fff4450b6a038beb5cab0ca763fee90f4706
Terdoo Nelson <nelsonterdoo1000@gmail.com>
2025-06-29 09:40:25 +0100

    added polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+from math import pi
+class Shape:
+
+    def area(self):
+        return NotImplementedError
+    
+
+
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+    
+
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    def area(self):
+        return pi*pow(self.radius,2)
